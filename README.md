ccd0c072afaa791cdf2338fcc9ef0288a30697d8
FenstonSingel <fenstonsingel@gmail.com>
2021-06-22 19:26:30 +0300

    Documented the evaluation attempt a little bit

--- README.md
+++ README.md
@@ -14,6 +14,17 @@ All options (backends, dir for results, etc.) specifies in bbf.conf file
 
 * The class `com.stepanov.bbf.coverage.CompilerInstrumentation` is duplicated in both the Java agent module and in the main project. Their contents should be kept identical.
 * `-javaagent:<path-to-instrumenter-jar>` VM option should be present in order for the compiler to be instrumented.
-* A new instrumenter jar can be assembled with the `kotlinc-instrumenter/jar` Gradle task. The new jar's default new position is the main project's `src/main/resources`.
+* A new instrumenter jar can be assembled with the `kotlinc-instrumenter/jar` Gradle task. The new jar's default parent directory is the main project's `src/main/resources`.
 
+### Notes on YouTrack dataset evaluation
 
+An attempt to evaluate the fault localization-based code deduplication method was made. The entry point can be found [here](src/test/kotlin/com/stepanov/bbf/isolation/testbed/IsolationTestbed.kt): it's a big mess in terms of configuring it, but oh well. Four parameters were deemed important enough to try varying:
+
+* whether starting samples were reduced or not
+* coverage level (method-wise or branch instruction-wise) of instrumentation
+* evaluation method of program entity's "suspiciousness"
+* a fraction of each bug's suspiciousness ranking that is used during duplicate search
+
+The dataset itself (in both original and reduced forms) can be found at `isolation-evaluation/samples-backup` along with the notes on how I process it a  bit. [This YouTrack crawler ](https://github.com/FenstonSingel/kt-youtrack-crawler) was used for obtaining the dataset. Copy the directory as `isolation-evaluation/samples` if you plan on using the testbed mentioned above.
+
+34.2 GiB (23.0 GiB in archived form) of intermediate artifacts such as mutants, coverages, and FL results are also (probably) available per request. The contents of the archive should be extracted to the `isolation-evaluation` folder for further use.
--- src/test/kotlin/com/stepanov/bbf/isolation/testbed/IsolationTestbed.kt
+++ src/test/kotlin/com/stepanov/bbf/isolation/testbed/IsolationTestbed.kt
@@ -17,6 +17,7 @@ fun main() {
 //    filterSamplesByBugPresence("isolation-evaluation/samples/youtrack", defaultBugInfo)
 //    reduceAllSamplesInDataset("isolation-evaluation/samples/youtrack", defaultBugInfo)
 
+    // GENERATION OF SETS OF MUTANTS FOR EACH DATASET SAMPLE
 //    MutantGenerator.generate(
 //        "isolation-evaluation/samples/youtrack",
 //        mutantsExportTag = "default",
@@ -25,20 +26,32 @@ fun main() {
 
 //    stacktraceEvaluation("isolation-evaluation/samples/youtrack-reduced")
 
+    // this is a fucking mess and i'm very sorry for bringing this into existence
     bugIsolationEvaluation(
         "isolation-evaluation/samples/youtrack-reduced", // choose original set or reduced one
-        BugIsolationSourceType.MUTANTS,
-        fractionOfRankingsConsidered = 1.0, // also a variable, duh
+        BugIsolationSourceType.MUTANTS, // pick a type of input data to work with
+        fractionOfRankingsConsidered = 1.0, // determine which fraction of each suspiciousness ranking (from top to bottom)
+                                            // is taken into consideration when comparing two bugs
+        // fine-tune which input data you want to work with
+        // make sure to specify all necessary tags for an input data type chosen earlier
         mutantsImportTag = "default",
 //        coveragesImportTag = "methods",
 //        resultsImportTag = "Ochiai",
 //        mutantsExportTag = "",
-        coveragesExportTag = "branches", // mark coverage type (methods/branches)
-        resultsExportTag = "Ochiai", // mark suspiciousness evaluation method)
+        // mark resulting coverage type (methods/branches)
+        // the actual coverage type should be changed manually in the java agent (recompile it as well)
+        // (see com.stepanov.bbf.coverage.instrumentation.PremainClass for that)
+        coveragesExportTag = "branches",
+        // mark used suspiciousness evaluation method
+        // the actual evaluation method should be changed manually
+        // (see com.stepanov.bbf.isolation.testbed.bugIsolationEvaluation / IsolationTestbedRuns.kt::73 for that)
+        resultsExportTag = "Ochiai",
+        // mark resulting fraction of each suspiciousness ranking taken into consideration
+        // the actual value was changed manually earlier
         comparisonsTag = "100%-ranking"
     )
 
-    // CALCULATING F-SCORES FOR STACKTRACE SIMILARITY RANKINGS
+    // CALCULATING F-SCORES FOR SIMILARITY RANKINGS
 //    val estimatesOriginal = loadEstimationResults("isolation-evaluation/samples/youtrack-results/stacktraces.cbor")
 //    val performancesOriginal = estimatesOriginal.calculateAllPossibleFScores(0.5)
 }
